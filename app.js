cfe15333299241fd415bd702a54753219eb1bc48
Alex Jones <alexander.jones@softwire.com>
2016-07-13 21:31:53 +0100

    Basic Setup Changes

--- app.js
+++ app.js
@@ -5,14 +5,15 @@ var express = require('express')
 , stylus = require('stylus')
 , nib = require('nib')
 , morgan = require('morgan')
-, path = require ('path');
+, path = require('path');
 
 var app = express()
 
 function compile(str, path) {
   return stylus(str)
     .set('filename', path)
-    .use(nib());
+    .use(nib())
+    .use(bootstrap());
 }
 
 app.set('views', __dirname + '/views')
@@ -20,15 +21,17 @@ app.set('view engine', 'pug')
 app.use(morgan('dev'))
 app.use(stylus.middleware(
   { src: __dirname + '/public'
-  , compile: compile
+    , compile: compile
   }
 ))
 app.use(express.static(path.join(__dirname + '/public')))
 
 app.get('/', function (req, res) {
   res.render('index',
-  { title : 'Home' }
-  )
+	     { title : 'Home' }
+	    )
 })
 
-app.listen(3000)
+app.listen(3000, '0.0.0.0', function() {
+  console.log('Listening to port:  ' + 3000);
+});
